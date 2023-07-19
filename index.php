0f72733fbc45bddd0c448c3ac57e9bbddba9dd1c
Nahima <nahima17.l@gmail.com>
2023-07-19 14:46:45 +0400

    refresh

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html lang="en">
-
+    
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +7,36 @@
     <title>BlogCookery_Island</title>
 </head>
 
-<body>
-    <header>
-        <navbar>
-            <button></button>
-            <button></button>
-            <button></button>
-        </navbar>
-    </header>
-</body>
+<header>
+    <navbar>
+    
+    </navbar>
+</header>
 
+<body>
+    <main>
+        <section class="card-container">
+            <h3>Entrées</h3>
+            <div class="containergrid">
+                <div class="card">
+                    <img src="Images\Crêpes_saumon.jpg" alt="Crêpes">
+                    <div class="text-card">
+                        <p>Difficulté</p>
+                        <p>Temps</p>
+                    </div>
+                    <h2>Crêpes-saumon</h2>
+                    <div class="img-card">
+                        <div class="logo-card">
+                            <img src="Images\icone_drapeau-bretagne.webp" alt="bretagne">
+                            <img src="Images\Icone-entrée.png" alt="entrée">
+                        </div>
+                        <button type="submit">
+                        Voir recettes
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
 </html>
