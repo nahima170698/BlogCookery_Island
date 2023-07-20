2ca0c9dac8072f73cee8dfb7471adc637c22a3f1
FourlinTerence <fourlinterence@gmail.com>
2023-07-20 13:09:11 +0400

    Navbar terminé

--- acceuil.php
+++ acceuil.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookery_Island Acceuil</title>
-    <link rel="shortcut icon" href="Images/Logo.Cookery_Island.png"/>
+    <link rel="shortcut icon" href="Images/Logo.Cookery_Island.png" />
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <nav>
@@ -14,9 +16,17 @@
             <a href="">Nos recettes</a>
             <a href="">Cours de cuisine</a>
             <a href="">Connexion</a>
-            <div><img class="logoProfil" src="Images/png-clipart-computer-icons-user-account-symbol-icon-mo-head-woman.png" alt=""></div>
+            <div class="dropdown">
+                <button class="dropbtn"><img class="logoProfil" src="Images/profil.png" alt=""></button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
         </nav>
     </header>
-    
+
 </body>
+
 </html>
