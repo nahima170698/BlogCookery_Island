271e9f7dc2af3b427c46a173bc05b244644977ac
FourlinTerence <fourlinterence@gmail.com>
2023-07-20 09:24:29 +0400

    Creation du fichier acceuil et creation de la navbar

---
+++ acceuil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookery_Island Acceuil</title>
+    <link rel="shortcut icon" href="Images/Logo.Cookery_Island.png"/>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href=""><img src="Images/Logo.Cookery_Island.png" alt=""></a>
+            <a href="">Nos recettes</a>
+            <a href="">Cours de cuisine</a>
+            <a href="">Connexion</a>
+            <div><img class="logoProfil" src="Images/png-clipart-computer-icons-user-account-symbol-icon-mo-head-woman.png" alt=""></div>
+        </nav>
+    </header>
+    
+</body>
+</html>
