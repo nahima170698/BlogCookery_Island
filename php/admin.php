be7b7429acf668e8ff4e11526c1d144e2338040d
miihar <mbae.am1@gmail.com>
2023-07-21 08:58:48 +0400

    entré dans la derniere phase de admin et connexion

---
+++ php/admin.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <p>nav a integrer avec include</p>
+    </header>
+
+    <main>
+        <section class="formConnexion">
+            <!-- form ajout recette -->
+            <form>
+                <h3>Ajouter une recette</h3>
+                <div class="mb-3">
+                    <label for="nomRecette" class="form-label">Nom recette</label>
+                    <input type="text" class="form-control" id="nomRecette" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="categorie" class="form-label">categorie</label>
+                    <input type="text" class="form-control" id="categorie" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="tempPreparation" class="form-label">Temps de Preparation</label>
+                    <input type="text" class="form-control" id="tempPreparation" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="difficulte" class="form-label">Difficulte</label>
+                    <input type="text" class="form-control" id="difficulte" aria-describedby="#">
+                </div>
+            </form>
+            <!-- form ajout ingredients -->
+            <form>
+                <h3>Ajouter des ingrédients</h3>
+                <div class="mb-3">
+                    <label for="ingredient1" class="form-label">Ingredient 1</label>
+                    <input type="text" class="form-control" id="ingredient1" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="ingredient2" class="form-label">Ingredient 2</label>
+                    <input type="text" class="form-control" id="ingredient2" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="ingredient3" class="form-label">Ingredient 3</label>
+                    <input type="text" class="form-control" id="ingredient3" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="ingredient4" class="form-label">Ingredient 4</label>
+                    <input type="text" class="form-control" id="ingredient4" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient5" class="form-label">Ingredient 5</label>
+                    <input type="text" class="form-control" id="ingredient5" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient6" class="form-label">Ingredient 6</label>
+                    <input type="text" class="form-control" id="ingredient6" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient7" class="form-label">Ingredient 7</label>
+                    <input type="text" class="form-control" id="ingredient7" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient8" class="form-label">Ingredient 8</label>
+                    <input type="text" class="form-control" id="ingredient8" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient9" class="form-label">Ingredient 9</label>
+                    <input type="text" class="form-control" id="ingredient9" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="ingredient10" class="form-label">Ingredient 10</label>
+                    <input type="text" class="form-control" id="ingredient10" aria-describedby="#">
+                </div>
+            </form>
+        
+        </section>
+
+        <section class="formCorps">
+            <!-- form ajout images + texte -->
+            <form>
+                <h3>Ajouter texte et images</h3>
+                <div class="mb-3">
+                    <label for="texte1" class="form-label">Texte 1</label>
+                    <input type="text" class="form-control" id="Texte1" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="image1" class="form-label">image 1</label>
+                    <input type="text" class="form-control" id="image1" aria-describedby="#">
+                </div>
+        
+                <div class="mb-3">
+                    <label for="texte2" class="form-label">texte 2</label>
+                    <input type="text" class="form-control" id="texte2" aria-describedby="#">
+                </div>
+                <div class="mb-3">
+                    <label for="image2" class="form-label">image 2</label>
+                    <input type="text" class="form-control" id="image 2" aria-describedby="#">
+                </div>
+            </form>
+        </section>
+    </main>
+
+
+    <footer>
+        <p>©Cookery_Island corporation. Tous droits réservés.</p>
+    </footer>
+
+
+
+
+
+
+</body>
+</html>
