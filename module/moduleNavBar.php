91cc17ae61e73ef9b09bc8066344389f1e776d2a
FourlinTerence <fourlinterence@gmail.com>
2023-07-21 10:30:04 +0400

    rejoint le dossier de abdourahim

--- module/moduleNavBar.php
+++ module/moduleNavBar.php
@@ -1,10 +1,10 @@
 <nav>
-    <a href=""><img src="Images/Logo.Cookery_Island.png" alt=""></a>
+    <a href=""><img src="../Images/Logo.Cookery_Island.png" alt=""></a>
     <a href="">Nos recettes</a>
     <a href="">Cours de cuisine</a>
     <a href="">Connexion</a>
     <div class="dropdown">
-        <button class="dropbtn"><img class="logoProfil" src="Images/profil.png" alt=""></button>
+        <button class="dropbtn"><img class="logoProfil" src="../Images/profil.png" alt=""></button>
         <div class="dropdown-content">
             <a href="#">Link 1</a>
             <a href="#">Link 2</a>
--- php/acceuil.php
+++ php/acceuil.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cookery_Island Acceuil</title>
-    <link rel="shortcut icon" href="Images/Logo.Cookery_Island.png" />
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="../Images/Logo.Cookery_Island.png" />
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
     <header>
-        <?php include "module/moduleNavBar.php"; ?>
+        <?php include "../module/moduleNavBar.php"; ?>
         <div class="header">
             <h1>Cookery Island</h1>
         </div>
@@ -22,13 +22,13 @@
             <div class="accueilPresentation">
                 <h2 class="accueilTitreBienvenue">Bienvenue chez Cookery Island</h2>
                 <div class="flexContainer">
-                    <img class="photoProfil" src="Images/Photo_profil.png" alt="photo de la proprietaire du blog">
+                    <img class="photoProfil" src="../Images/Photo_profil.png" alt="photo de la proprietaire du blog">
                     <p class="accueilTextDescriptionBlog">Presentation du blog rapide
                         Cupcake ipsum dolor sit amet cake bear claw oat cake caramels. Apple pie sweet sweet ice cream tart tootsie roll. Dragée gummi bears pastry shortbread ice cream caramels biscuit apple pie ice cream. Chocolate cake pudding cake muffin tart sugar plum pudding soufflé. Biscuit cupcake macaroon candy bear claw donut</p>
                 </div>
             </div>
             <div class="accueilPresentation" id="accueilPortrait">
-                <img src="Images/Logo.Cookery_Island.png" alt="">
+                <img src="../Images/Logo.Cookery_Island.png" alt="">
                 <p class="accueilTextDescriptionCookeryIsland">Body Presentation de Cookery Island (type de cuisines, service etc)
                     Cupcake ipsum dolor sit amet cake bear claw oat cake caramels. Apple pie sweet sweet ice cream tart tootsie roll. Dragée gummi bears pastry shortbread ice cream caramels biscuit apple pie ice cream. Chocol</p>
             </div>
@@ -37,7 +37,7 @@
             <h2>Nos meilleurs recettes</h2>
             <div class="carteContainer">
                 <div class="carte">
-                    <img class="carteImageContainer" src="Images/Breakfast.jpg" alt="">
+                    <img class="carteImageContainer" src="../Images/Breakfast.jpg" alt="">
                     <div class="difficulteTempsRecette">
                         <p>Facile</p>
                         <p>30 min</p>
@@ -45,14 +45,14 @@
                     <h3>Breakfast</h3>
                     <div class="difficulteTempsRecette">
                         <div class="iconeContainer">
-                            <img class="icone" src="Images/istockphoto-1183041351-612x612-modified.png" alt="">
-                            <img class="icone" src="Images/istockphoto-1183041351-612x612-modified.png" alt="">
+                            <img class="icone" src="../Images/istockphoto-1183041351-612x612-modified.png" alt="">
+                            <img class="icone" src="../Images/istockphoto-1183041351-612x612-modified.png" alt="">
                         </div>
                         <input class="boutonCarte" type="button" value="Voir article">
                     </div>
                 </div>
                 <div class="carte">
-                    <img class="carteImageContainer" src="Images/Pizza.jpg" alt="">
+                    <img class="carteImageContainer" src="../Images/Pizza.jpg" alt="">
                     <div class="difficulteTempsRecette">
                         <p>Moyen</p>
                         <p>120 min</p>
@@ -60,14 +60,15 @@
                     <h3>Pizza</h3>
                     <div class="difficulteTempsRecette">
                         <div class="iconeContainer">
-                            <img class="icone" src="Images/iconePlat (2)-modified.png" alt="">
-                            <img class="icone" src="Images/iconePlat (2)-modified.png" alt="">
+                            <img class="icone" src="../Images/iconePlat (2)-modified.png" alt="">
+                            <img class="icone" src="../Images/iconePlat (2)-modified.png" alt="">
+                            
                         </div>
                         <input class="boutonCarte" type="button" value="Voir article">
                     </div>
                 </div>
                 <div class="carte">
-                    <img class="carteImageContainer" src="Images/MilleFeulle.jpg" alt="">
+                    <img class="carteImageContainer" src="../Images/MilleFeulle.jpg" alt="">
                     <div class="difficulteTempsRecette">
                         <p>Difficile</p>
                         <p>210 min</p>
@@ -75,8 +76,8 @@
                     <h3>Mille Feulle</h3>
                     <div class="difficulteTempsRecette">
                         <div>
-                            <img class="icone" src="Images/dessert-modified.png" alt="">
-                            <img class="icone" src="Images/dessert-modified.png" alt="">
+                            <img class="icone" src="../Images/dessert-modified.png" alt="">
+                            <img class="icone" src="../Images/dessert-modified.png" alt="">
                         </div>
                         <input class="boutonCarte" type="button" value="Voir article">
                     </div>
@@ -87,7 +88,7 @@
         <section class="acceuilSectionCours">
             <h2 class="acceuilCoursTitre">Reservez vos places pour nos cours de cuisines</h2>
             <div class="flexContainer">
-                <img class="acceuilImageCours" src="Images/coursCuisinerogné.jpg" alt="Cours de cuisine">
+                <img class="acceuilImageCours" src="../Images/coursCuisinerogné.jpg" alt="Cours de cuisine">
                 <div class="acceuilTextCours">
                     <p>Liste de style de cuisine disponible
                         Cupcake ipsum dolor sit amet cake bear claw oat cake caramels. Apple pie sweet sweet ice cream tart tootsie roll. Dragée gummi</p>
@@ -102,7 +103,7 @@
     </main>
     <footer>
         <?php
-            include "module/moduleFooter.php";
+            include "../module/moduleFooter.php";
         ?>
     </footer>
 </body>
