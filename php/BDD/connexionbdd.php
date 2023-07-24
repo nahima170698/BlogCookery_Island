916fe6008eb03fa580d6a4dd869a533108163824
miihar <mbae.am1@gmail.com>
2023-07-24 12:59:39 +0400

    tout les fonction ajout sont ok

--- php/BDD/connexionbdd.php
+++ php/BDD/connexionbdd.php
@@ -43,16 +43,20 @@ class MaConnexion{
         }
     }
 
-    public function insertionRecette($nomRecette,$categorie,$tempsPrepa,$difficulte){
+    public function insertionRecette($nomRecette,$categorie,$tempsPrepa,$difficulte,$texte_un,$texte_deux,$image_un,$image_deux){
         try {
-            $requete = " INSERT INTO recette(Nom_Recette, Categorie, Temps_Preparation,Difficulte)
-                VALUES (:Nom_Recette, :Categorie, :Temps_Preparation,:Difficulte)" ;
+            $requete = " INSERT INTO recette(Nom_Recette, Categorie, Temps_Preparation,Difficulte,Texte_un, Texte_deux, Image_un,Image_deux)
+                VALUES (:Nom_Recette, :Categorie, :Temps_Preparation,:Difficulte,:Texte_un, :Texte_deux, :Image_un,:Image_deux)" ;
             $requete_preparee = $this->connexionPDO->prepare($requete);
 
             $requete_preparee->bindParam(':Nom_Recette',$nomRecette,PDO::PARAM_STR,30);
             $requete_preparee->bindParam(':Categorie',$categorie,PDO::PARAM_STR,50);
             $requete_preparee->bindParam(':Temps_Preparation',$tempsPrepa,PDO::PARAM_STR);
             $requete_preparee->bindParam(':Difficulte',$difficulte,PDO::PARAM_STR);
+            $requete_preparee->bindValue(':Texte_un',$texte_un,PDO::PARAM_STR);
+            $requete_preparee->bindValue(':Texte_deux',$texte_deux,PDO::PARAM_STR);
+            $requete_preparee->bindValue(':Image_un',$image_un,PDO::PARAM_STR);
+            $requete_preparee->bindValue(':Image_deux',$image_deux,PDO::PARAM_STR);
 
             $requete_preparee->execute();
             echo ("insertion reussi");
@@ -62,12 +66,15 @@ class MaConnexion{
             return $e->getMessage();
         }
     }
-    public function insertionIngredient($ingredient_un,$ingredient_deux,$ingredient_trois,$ingredient_quatre,$ingredient_cinq,$ingredient_six,$ingredient_sept,$ingredient_huit,$ingredient_neuf,$ingredient_dix){
-        try {
-            $requete = " INSERT INTO client(Ingredient_1,Ingredient_2,Ingredient_3,Ingredient_4,Ingredient_5,Ingredient_6,Ingredient_7,Ingredient_8,Ingredient_9,Ingredient_10)
-                VALUES (:Ingredient_1,:Ingredient_2,:Ingredient_3,:Ingredient_4,:Ingredient_5,:Ingredient_6,:Ingredient_7,:Ingredient_8,:Ingredient_9,:Ingredient_10)" ;
-            $requete_preparee = $this->connexionPDO->prepare($requete);
 
+    public function insertionIngredient($idrecette,$ingredient_un,$ingredient_deux,$ingredient_trois,$ingredient_quatre,$ingredient_cinq,$ingredient_six,$ingredient_sept,$ingredient_huit,$ingredient_neuf,$ingredient_dix,$ingredient_onze,$ingredient_douze,$ingredient_treize,$ingredient_quatorze,$ingredient_quinz){
+
+        try {
+            $requete = " INSERT INTO ingredient(ID_Recette,Ingredient_1,Ingredient_2, Ingredient_3,Ingredient_4,Ingredient_5, Ingredient_6, Ingredient_7,Ingredient_8,Ingredient_9,Ingredient_10,Ingredient_11,Ingredient_12,Ingredient_13,Ingredient_14,Ingredient_15)
+            VALUES (:ID_Recette,:Ingredient_1,:Ingredient_2, :Ingredient_3,:Ingredient_4,:Ingredient_5, :Ingredient_6, :Ingredient_7,:Ingredient_8,:Ingredient_9,:Ingredient_10,:Ingredient_11,:Ingredient_12,:Ingredient_13,:Ingredient_14,:Ingredient_15)" ;
+            $requete_preparee = $this->connexionPDO->prepare($requete);
+            
+            $requete_preparee->bindParam(':ID_Recette',$idrecette,PDO::PARAM_STR,30);
             $requete_preparee->bindParam(':Ingredient_1',$ingredient_un,PDO::PARAM_STR,30);
             $requete_preparee->bindParam(':Ingredient_2',$ingredient_deux,PDO::PARAM_STR,50);
             $requete_preparee->bindParam(':Ingredient_3',$ingredient_trois,PDO::PARAM_STR);
@@ -78,6 +85,12 @@ class MaConnexion{
             $requete_preparee->bindParam(':Ingredient_8',$ingredient_huit,PDO::PARAM_STR);
             $requete_preparee->bindParam(':Ingredient_9',$ingredient_neuf,PDO::PARAM_STR);
             $requete_preparee->bindParam(':Ingredient_10',$ingredient_dix,PDO::PARAM_STR);
+            $requete_preparee->bindParam(':Ingredient_11',$ingredient_onze,PDO::PARAM_STR,30);
+            $requete_preparee->bindParam(':Ingredient_12',$ingredient_douze,PDO::PARAM_STR,30);
+            $requete_preparee->bindParam(':Ingredient_13',$ingredient_treize,PDO::PARAM_STR,30);
+            $requete_preparee->bindParam(':Ingredient_14',$ingredient_quatorze,PDO::PARAM_STR,30);
+            $requete_preparee->bindParam(':Ingredient_15',$ingredient_quinz,PDO::PARAM_STR,30);
+
 
             $requete_preparee->execute();
             echo ("insertion reussi");
@@ -87,15 +100,17 @@ class MaConnexion{
             return $e->getMessage();
         }
     }
-    public function insertionUtilisateur($nom,$prenom,$identifiant,$mdp,$id){
+
+
+    public function insertionUtilisateur($nom,$prenom,$pseudo,$mdp,$id){
         try {
-            $requete = " INSERT INTO utilisateur(Nom, Prenom, Identifiant,Mot_De_Passe,ID_Role)
-                VALUES (:Nom, :Prenom, :Identifiant, :Mot_De_Passe,:ID_Role)";
+            $requete = " INSERT INTO utilisateur(Nom, Prenom,Pseudo,Mot_De_Passe,ID_Role)
+                VALUES (:Nom, :Prenom, :Pseudo, :Mot_De_Passe,:ID_Role)";
             $requete_preparee = $this->connexionPDO->prepare($requete);
 
             $requete_preparee->bindParam(':Nom',$nom,PDO::PARAM_STR,30);
-            $requete_preparee->bindParam(':Categorie',$prenom,PDO::PARAM_STR,50);
-            $requete_preparee->bindParam(':Identifiant',$identifiant,PDO::PARAM_STR);
+            $requete_preparee->bindParam(':Prenom',$prenom,PDO::PARAM_STR,50);
+            $requete_preparee->bindParam(':Pseudo',$pseudo,PDO::PARAM_STR);
             $requete_preparee->bindParam(':Mot_De_Passe',$mdp,PDO::PARAM_STR);
             $requete_preparee->bindParam(':ID_Role',$id,PDO::PARAM_STR);
 
@@ -109,10 +124,11 @@ class MaConnexion{
     }
 
     // Fonction de mis à jour du statut de la salle dans la table Salle 
-    public function maj_Recette($nomRecette,$categorie,$tempsPrepa,$difficulte){
+    public function maj_Recette($nomRecette,$categorie,$tempsPrepa,$difficulte,$texte_un,$texte_deux,$image_un,$image_deux){
         try {
 
-            $requete = "UPDATE recette SET Nom_Recette = ?, Categorie = ?,Temps_Preparation = ?, Difficulte = ?
+            $requete = "UPDATE recette SET Nom_Recette = ?, Categorie = ?,Temps_Preparation = ?, Difficulte = ?,
+                Texte_un = ?,Texte_deux = ?,Image_un = ?,Image_deux = ?
                 WHERE ID_Recette = ?";
 
             $requete_preparee = $this->connexionPDO->prepare($requete);
@@ -121,6 +137,11 @@ class MaConnexion{
             $requete_preparee->bindValue(2,':Categorie',$categorie,PDO::PARAM_STR,50);
             $requete_preparee->bindValue(3,':Temps_Preparation',$tempsPrepa,PDO::PARAM_STR);
             $requete_preparee->bindValue(4,':Difficulte',$difficulte,PDO::PARAM_STR);
+            $requete_preparee->bindValue(5,':Texte_un',$texte_un,PDO::PARAM_STR);
+            $requete_preparee->bindValue(6,':Texte_deux',$texte_deux,PDO::PARAM_STR);
+            $requete_preparee->bindValue(7,':Image_un',$image_un,PDO::PARAM_STR);
+            $requete_preparee->bindValue(8,':Image_deux',$image_deux,PDO::PARAM_STR);
+
 
             $requete_preparee->execute();
 
@@ -149,13 +170,17 @@ class MaConnexion{
     }
 }
 
-/*
-$test = new MaConnexion("reservsalle", "", "root", "localhost");
 
-$inser = $test->insertionRecette("doe","john","mail@mail.co");
-var_dump($inser);
-$sallee = $test->maj_Statut("Salle Madrid");
-var_dump($sallee);
-*/
+
+$test = new MaConnexion("cookery_island", "", "root", "localhost");
+
+//$inser = $test->insertionRecette("carry zebre","plat","15 min","Facile","text random","texte random encore", "img","img2");
+//var_dump($inser);
+$inserting = $test->insertionIngredient(1,"ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré","ingré");
+var_dump($inserting);
+// $maj = $test->maj_Recette("");
+// $insertuti = $test->insertionUtilisateur("jon","snow","king of the north","lovemyaunt",1);
+// var_dump($insertuti);
+
 
 ?>
