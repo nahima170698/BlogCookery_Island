7ac7825fbd444d83f185674969de59afae51ecf4
Naima <nahima17.l@gmail.com>
2023-07-26 11:30:35 +0400

    users

--- php/BDD/insertionusers.php
+++ php/BDD/insertionusers.php
@@ -5,4 +5,6 @@ $connexionbdd = new MaConnexion ("cookery_island","","root","localhost");
 $connexionbdd->insertionUtilisateur($_POST['Nom'],$_POST['Prenom'],$_POST['AdresseEmail'], $_POST['Pseudo'],$_POST['Mot_De_Passe'],$_POST['ID_Role']);
 
 header("Location: ../admin.php");
+
+
 ?>
